68fece45ed380d3343928f3a13a79c5c953bf15c
Udit Vikhe <udit.vikhe@gmail.com>
2024-11-04 07:43:46 +0530

    First Element Not Smaller Than Target (Easy)

---
+++ first_not_smaller.py
@@ -0,0 +1,14 @@
+def first_not_smaller(arr, target):
+    left, right = 0, len(arr) - 1
+    boundary_index = -1
+
+    while (left <= right):
+        mid = (left+right)//2
+        if arr[mid] >= target:
+            boundary_index = mid
+            right = mid - 1
+        else:
+            left = mid + 1
+    return boundary_index
+
+print(first_not_smaller([1,3,3,5,8,8,10], 2))
